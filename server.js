e729b90fa6386b40885995b1b16b05e209794abd
SeanSassenrath <sean.sassenrath@gmail.com>
2015-09-27 21:07:55 -0700

    setting up bower components

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ var port = 3000;
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + '/public'));
 
-
 app.get('/', function(req, res) {
     res.render("index.ejs", {
         greeting: "Hello World"
